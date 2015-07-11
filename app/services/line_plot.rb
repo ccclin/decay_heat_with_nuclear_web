3d30995d752994def269b63f1aa16d6935499aae
C.lin <demeter.yeh@gmail.com>
2015-07-11 16:28:43 +0800

    [TITLE] add `asb9-2` `P/P0 without k`

--- app/services/line_plot.rb
+++ app/services/line_plot.rb
@@ -11,11 +11,22 @@ class LinePlot
         f.options[:yAxis] = { title: { text: "#{Y_NAME}" }, min: 0 }
         data_array = {}
         hash.each_key do |key|
-          data_array[key] = []
-          hash[key][:ts].each_index do |i|
-            data_array[key] << [hash[key][:ts][i].to_f, hash[key][:p_p0][i].to_f]
+          if key == :asb9_2
+            data_array[key] = []
+            data_array[:asb9_2_without_K] = []
+            hash[key][:ts].each_index do |i|
+              data_array[key] << [hash[key][:ts][i].to_f, hash[key][:p_p0][i].to_f]
+              data_array[:asb9_2_without_K] << [hash[key][:ts][i].to_f, hash[key][:p_p0_without_k][i].to_f]
+            end
+            f.series(type: 'line', name: "#{key}", data: data_array[key])
+            f.series(type: 'line', name: "#{key}_without_k", data: data_array[key])            
+          else
+            data_array[key] = []
+            hash[key][:ts].each_index do |i|
+              data_array[key] << [hash[key][:ts][i].to_f, hash[key][:p_p0][i].to_f]
+            end
+            f.series(type: 'line', name: "#{key}", data: data_array[key])
           end
-          f.series(type: 'line', name: "#{key}", data: data_array[key])
         end
       end
       chart
