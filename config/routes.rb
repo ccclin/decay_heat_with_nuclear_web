1a026e3b220537853d3025dbdd04369d93354259
C.lin <demeter.yeh@gmail.com>
2015-07-15 10:24:09 +0800

    [TITLE] disable download feature
    [CHANGES]
    - disable `download` feature
    - enable `show data` feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'decay_heat#index'
+  get 'calculate' => 'decay_heat#calculate'
   post 'calculate' => 'decay_heat#calculate'
-  get 'download' => 'decay_heat#download'
+  # get 'download' => 'decay_heat#download'
+  get 'show' => 'decay_heat#show'
 end
